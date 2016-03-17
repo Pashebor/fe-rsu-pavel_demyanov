e81208f3225cdd08a0d43e6482d915665754c985
Pashebor <pashebor@gmail.com>
2016-03-17 19:24:57 +0400

    anothher commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var firstName = 'pavel', lastName = 'demyanov', age = '13';
+
+function showNames(){
+    alert(firstName);
+    alert(lastName);
+    alert(age);
+}
+function headerClick(){
+    alert("some text");
+}
